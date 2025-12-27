0c43a32d40e113d1c9d0ea0c2c2a49010c86ea48
scarletpiguave28-ops <scarletpiguave28@gmail.com>
2025-12-26 20:42:18 -0500

    Refactor form fields and update submit button

--- resources/views/autores/edit.blade.php
+++ resources/views/autores/edit.blade.php
@@ -6,25 +6,38 @@
         <i class="bi bi-pencil-square"></i> Editar Autor
     </div>
     <div class="card-body">
-     <form action="{{ route('autores.update', $autore) }}" method="POST">
+        <form action="{{ route('autores.update', $autore) }}" method="POST">
+            @csrf
+            @method('PUT')
             <div class="row mb-3">
                 <div class="col-md-4">
                     <label class="form-label">Nombre Real</label>
-                    <input type="text" name="nombre_real" class="form-control" value="{{ old('nombre_real', $autor->nombre_real) }}" required>
+                    <input type="text" name="nombre_real" class="form-control"
+                           value="{{ old('nombre_real', $autore->nombre_real) }}" required>
                 </div>
                 <div class="col-md-4">
                     <label class="form-label">Email</label>
-                    <input type="email" name="email" class="form-control" value="{{ old('email', $autor->email) }}" required>
+                    <input type="email" name="email" class="form-control"
+                           value="{{ old('email', $autore->email) }}" required>
                 </div>
                 <div class="col-md-4">
                     <label class="form-label">País</label>
-                    <input type="text" name="pais" class="form-control" value="{{ old('pais', $autor->pais) }}" required>
+                    <input type="text" name="pais" class="form-control"
+                           value="{{ old('pais', $autore->pais) }}" required>
                 </div>
             </div>
+
             <div class="mb-3">
                 <label class="form-label">Seudónimo</label>
-                <input type="text" name="seudonimo[nombre_pluma]" class="form-control"
-                       value="{{ old('seudonimo.nombre_pluma', $autor->seudonimo->nombre_pluma ?? '') }}" placeholder="Opcional">
+                <input type="text" name="nombre_pluma" class="form-control"
+                       value="{{ old('nombre_pluma', $autore->nombre_pluma ?? '') }}" placeholder="Opcional">
             </div>
-            <button type="submit" class="btn btn-success"><i class="bi bi-check-circle"></i> Actualizar</button>
+
+            <button type="submit" class="btn btn-success">
+                <i class="bi bi-check-circle"></i> Actualizar
+            </button>
             <a href="{{ route('autores.index') }}" class="btn btn-secondary">Cancelar</a>
+        </form>
+    </div>
+</div>
+@endsection
